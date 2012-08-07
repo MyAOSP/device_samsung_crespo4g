787a7f44a5bc1b8ebbec14c439064c3b1769ad83
travp624 <travp624@gmail.com>
2012-08-07 13:56:03 -0500

    merge in changes from android-4.1.1_r4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,16 +17,12 @@
 # Product-specific compile-time definitions.
 #
 
-# Set this up here so that BoardConfigVendor.mk can override it
-BOARD_USES_GENERIC_AUDIO := false
-
-BOARD_USES_LIBSECRIL_STUB := true
-
-BOARD_NO_PAGE_FLIPPING := true
-BOARD_NO_32BPP := true
-
+# Use the non-open-source parts, if they're present
 -include vendor/samsung/crespo/BoardConfig4gVendor.mk
-include device/samsung/crespo/BoardConfigCommon.mk
+
+# Use the open-source board-info file if none was set by the vendor
+# config.
+TARGET_BOARD_INFO_FILE ?= device/samsung/crespo4g/board-info.txt
 
 # Use the parts that are common between all crespos
 include device/samsung/crespo/BoardConfigCommon.mk
